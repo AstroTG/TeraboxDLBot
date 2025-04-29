425e0c71dbbf8dc94b967dd3806fc69e840c2f84
AstroTG <4xrikrishnan.py@gmail.com>
2025-04-29 11:15:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 import time
 
+import telethon
+from telethon import TelegramClient
 import humanreadable as hr
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
@@ -9,14 +11,67 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from config import ADMINS, API_HASH, API_ID, BOT_TOKEN, BOT_USERNAME, FORCE_LINK
 from redis_db import db
 from send_media import VideoSender
-from tools import generate_shortenedUrl, is_user_on_chat, remove_all_videos
 
 log = logging.getLogger(__name__)
 
-bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+def generate_shortenedUrl(
+    sender_id: int,
+):
+    try:
+        uid = str(uuid.uuid4())
+        data = requests.get(
+            "https://gplinks.com/member/tools/api",
+            params={
+                "api": GP_LINKS_API,
+                "url": f"https://t.me/{BOT_USERNAME}?start=token_{uid}",
+                "alias": uid.split("-", maxsplit=2)[0],
+            },
+        )
+        data.raise_for_status()
+        data_json = data.json()
+        if data_json.get("status") == "success":
+            url = data_json.get("shortenedUrl")
+            db.set(f"token_{uid}", f"{sender_id}|{url}", ex=21600)
+            return url
+        else:
+            return None
+    except Exception as e:
+        return None
 
+async def is_user_on_chat(bot: TelegramClient, chat_id: int, user_id: int) -> bool:
+    """
+    Check if a user is present in a specific chat.
 
-@bot.on(filters.command("start") & filters.private)
+    Parameters:
+        bot (TelegramClient): The Telegram client instance.
+        chat_id (int): The ID of the chat.
+        user_id (int): The ID of the user.
+
+    Returns:
+        bool: True if the user is present in the chat, False otherwise.
+    """
+    try:
+        check = await bot.get_permissions(chat_id, user_id)
+        return check
+    except Exception:
+        return False
+
+def remove_all_videos():
+    current_directory = os.getcwd()
+
+    video_extensions = [".mp4", ".mkv", ".webm"]
+
+    try:
+        for file_name in os.listdir(current_directory):
+            if any(file_name.lower().endswith(ext) for ext in video_extensions):
+                file_path = os.path.join(current_directory, file_name)
+
+                os.remove(file_path)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+@bot.on_message(filters.command("start") & filters.private)
 async def start(client: Client, message: Message):
     reply_text = """
 Hello there! I'm your friendly video downloader bot specially designed to fetch videos from Terabox. Share the Terabox link with me, and I'll swiftly get started on downloading it for you.
@@ -26,29 +81,11 @@ Let's make your video experience even better!
     await message.reply_text(
         reply_text,
         disable_web_page_preview=True,
-        parse_mode="markdown",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Website Source Code",
-                        url="https://github.com/r0ld3x/terabox-app",
-                    ),
-                    InlineKeyboardButton(
-                        "Bot Source Code",
-                        url="https://github.com/r0ld3x/terabox-downloader-bot",
-                    ),
-                ],
-                [
-                    InlineKeyboardButton("Channel ", url="https://t.me/RoldexVerse"),
-                    InlineKeyboardButton("Group ", url="https://t.me/RoldexVerseChats"),
-                ],
-            ]
-        ),
+        parse_mode="markdown"
     )
 
 
-@bot.on(filters.command("gen") & filters.private)
+@bot.on_message(filters.command("gen") & filters.private)
 async def generate_token(client: Client, message: Message):
     is_user_active = db.get(f"active_{message.from_user.id}")
     if is_user_active:
@@ -87,13 +124,7 @@ Keep the interactions going smoothly! 😊
         ),
     )
 
-@bot.on(
-    filters.command("start")
-    & filters.private
-    & filters.regex(
-        r"(?!token_)([0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})"
-    )
-)
+@bot.on_message(filters.command("start") & filters.private & filters.regex(r"(?!token_)([0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})"))
 async def start_ntoken(client: Client, message: Message):
     uuid_match = re.search(
         r"(?!token_)([0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})",
@@ -120,13 +151,7 @@ async def start_ntoken(client: Client, message: Message):
         return await message.reply_text("""your requested file is not available.""")
 
 
-@bot.on(
-    filters.command("start")
-    & filters.private
-    & filters.regex(
-        r"token_([0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})"
-    )
-)
+@bot.on_message(filters.command("start") & filters.private & filters.regex(r"token_([0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})"))
 async def start_token(client: Client, message: Message):
     uuid_match = re.search(
         r"token_([0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})",
@@ -145,7 +170,7 @@ async def start_token(client: Client, message: Message):
         )
           if not check_if:
         return await message.reply_text(
-            "You haven't joined @RoldexVerse or @RoldexVerseChats yet. Please join the channel and then send me the link again.\nThank you!",
+            "You haven't joined @AstroBotz or @AstroBotzSupport yet. Please join the channel and then send me the link again.\nThank you!",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -193,35 +218,21 @@ Your session will expire in {t.to_humanreadable()}."""
       
 
 
-@bot.on(
-    events.NewMessage(
-        pattern="/remove (.*)",
-        incoming=True,
-        outgoing=False,
-        from_users=ADMINS,
-    )
-)
-async def remove(m: UpdateNewMessage):
-    user_id = m.pattern_match.group(1)
+@bot.on(filters.command("remove") & filters.user(ADMINS) & filters.text)
+async def remove(client: Client, message: Message):
+    try:
+        user_id = message.text.split(" ")[1]  # Extract user ID from command
+    except IndexError:
+        return await message.reply_text("Please provide a user ID.")
+
     if db.get(f"check_{user_id}"):
         db.delete(f"check_{user_id}")
-        await m.reply(f"Removed {user_id} from the list.")
+        await message.reply_text(f"Removed {user_id} from the list.")
     else:
-        await m.reply(f"{user_id} is not in the list.")
+        await message.reply_text(f"{user_id} is not in the list.")
 
 
-@bot.on(
-    events.NewMessage(
-        pattern="/removeall",
-        incoming=True,
-        outgoing=False,
-        from_users=ADMINS,
-    )
-)
-async def removeall(m: UpdateNewMessage):
+@bot.on(filters.command("removeall") & filters.user(ADMINS))
+async def removeall(client: Client, message: Message):
     remove_all_videos()
-    return await m.reply("Removed all videos from the list.")
-
-
-bot.start(bot_token=BOT_TOKEN)
-bot.run_until_disconnected()
+    await message.reply_text("Removed all videos from the list.")
